665db08e9e614ff4e30b67a34c9428c4b5ce002d
KiraBurova <coca-cola-from-hell@yandex.ru>
2018-10-02 00:28:41 +0300

    add redux folders

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import {SearchInput} from '../UI/SearchInput/SearchInput';
-
 import './Menu.css';
 
 export const Menu = () => (
