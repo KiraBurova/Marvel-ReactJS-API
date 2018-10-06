a68f44959c1a0eedabbb27b0e15059462e7f516d
KiraBurova <coca-cola-from-hell@yandex.ru>
2018-10-06 16:57:05 +0300

    add navbar component to the main page

--- src/screens/Main/Main.jsx
+++ src/screens/Main/Main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import {Menu} from '../../components/Menu/Menu';
-
+import { Menu } from '../../components/Menu/Menu';
+import { Navbar } from '../../components/UI/Navbar/Navbar';
 import './Main.css';
-
 
 export const ScreensMain = () => (
     <section className="main">
-        <h1 className="main__header">Marvel</h1>
+        <Navbar text={'Marvel'}/>
         <Menu />
     </section>
 );
