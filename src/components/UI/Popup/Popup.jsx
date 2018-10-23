a1194509674845f3bfd572daeb55946acc28a944
Kira <kira@nativeroll.tv>
2018-10-23 17:00:10 +0300

    work on popup

--- src/components/UI/Popup/Popup.jsx
+++ src/components/UI/Popup/Popup.jsx
@@ -4,14 +4,15 @@ import './Popup.css';
 
 export const Popup = ({info}) => (
     <div>
-    <div className="overlay"></div>
-    <div className="popup">
-        <div className="card">
-        <div className="card-image">
-            <img src={`${info.thumbnail.path}.${info.thumbnail.extension}`} alt={info.title}></img>
-            <span className='card-title'>{info.title}</span>
+        <div className="overlay"></div>
+        <div className="popup">
+            <i className="material-icons">close</i>
+            <div className="card">
+                <div className="card-image">
+                    <img src={`${info.thumbnail.path}.${info.thumbnail.extension}`} alt={info.title}></img>
+                    <span className='card-title'>{info.title}</span>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 );
