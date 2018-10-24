7d7a33b8d9ba6f3ce047ebe15c6e5140850eefc8
Kira <kira@nativeroll.tv>
2018-10-24 12:10:46 +0300

    Updates

---
+++ precache-manifest.1e32411466073582aab2d4d0ec016ea5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1c82c902b7ece10ba04b318bfc6d899",
+    "url": "/Marvel-ReactJS-API/static/media/palooka-bb.regular.b1c82c90.ttf"
+  },
+  {
+    "revision": "724ecaa8728d6f173793",
+    "url": "/Marvel-ReactJS-API/static/js/runtime~main.724ecaa8.js"
+  },
+  {
+    "revision": "857f9e490fefb25ddce2",
+    "url": "/Marvel-ReactJS-API/static/js/main.857f9e49.chunk.js"
+  },
+  {
+    "revision": "a15b8bb8824c54821929",
+    "url": "/Marvel-ReactJS-API/static/js/1.a15b8bb8.chunk.js"
+  },
+  {
+    "revision": "857f9e490fefb25ddce2",
+    "url": "/Marvel-ReactJS-API/static/css/main.8e6c4147.chunk.css"
+  },
+  {
+    "revision": "a15b8bb8824c54821929",
+    "url": "/Marvel-ReactJS-API/static/css/1.d3724a49.chunk.css"
+  },
+  {
+    "revision": "851ee87e94303a9f47ce9f85747a3598",
+    "url": "/Marvel-ReactJS-API/index.html"
+  }
+];
